6992ea46f2417d1fc680a33db40fdc3d772f044e
Dominik Zsiros <zsdominik@outlook.com>
2019-11-25 17:16:57 +0100

    init

---
+++ src/test/java/com/mssql/beadando/BeadandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mssql.beadando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeadandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
