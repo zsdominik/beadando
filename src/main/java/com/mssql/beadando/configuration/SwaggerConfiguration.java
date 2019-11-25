6ec54d57c88d2c1061feed2f7b979c3c1e466112
Dominik Zsiros <zsdominik@outlook.com>
2019-11-25 17:46:21 +0100

    swagger added

---
+++ src/main/java/com/mssql/beadando/configuration/SwaggerConfiguration.java
@@ -0,0 +1,32 @@
+package com.mssql.beadando.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("net.guides.springboot2.springboot2swagger2.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("MSSQL Demo REST API")
+                .version("1.0.0")
+                .build();
+    }
+}
